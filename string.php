6130d20fd8710b5bd7f61b05f36e2194fb6c8974
chaewon <doingengineer@gmail.com>
2022-01-03 23:57:00 +0900

    second commit

---
+++ string.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<body>
+<h1>String & String operator</h1>
+<?php 
+echo 'hello \"w\"orld';
+?>
+
+<h2>concatenation operator</h2>
+<?php
+echo "hello "."world"; //좌항과 우항을 결합한다.
+?>
+
+
+</body>
+</html>
