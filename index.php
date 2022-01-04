7b66b0114e7c7a46df3f64b992fb8a1b5259eeaa
chaewon <doingengineer@gmail.com>
2022-01-04 19:13:44 +0900

    second commit

--- index.php
+++ index.php
@@ -7,7 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-<?php echo date('Y-m-d H:i:s'); 
-?>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+        echo $_GET['id'];
+        ?>
+    </h2>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum in ea doloremque. Numquam aliquid et non modi vel. Corporis explicabo ex obcaecati ratione optio veniam ullam doloremque dicta cumque reiciendis.
+
 </body>
 </html>
