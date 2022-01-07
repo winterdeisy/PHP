ec740892c8c10cab894d37ddd187dc9a554ba417
chaewon <doingengineer@gmail.com>
2022-01-07 23:50:30 +0900

    Second

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
 </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
+    <ol>
+        <?php
+        $list = scandir('data');
+        var_dump($list);
+        ?>
+    </ol>
     <?php
     // data 디렉토리에 있는 파일의 목록을 가져오고
     // 그 파일의 목록 하나하나를 li와 a 태그로 글목록 만들기
