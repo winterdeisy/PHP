ad699fbfb948d91446973e6720c7d581d139bdb3
chaewon <doingengineer@gmail.com>
2022-01-08 22:02:32 +0900

    function

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+    <?php    
+    print_title();
+    ?>
+    </title>
 </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
@@ -39,6 +43,11 @@
         echo "hello, php";
     }
     ?>
-
+    <?php
+    print_title();
+    ?>
+    <?php
+    print_description();
+    ?>
 </body>
 </html>
