8283192fea127d1564ecb92d091c53e45614b1fd
chaewon <doingengineer@gmail.com>
2022-01-08 21:36:40 +0900

    first

--- index.php
+++ index.php
@@ -8,46 +8,18 @@
 </head>
 <body>
     <h1><a href="index.php">WEB</a></h1>
-    <ol>
-        <?php
-        $list = scandir('data');
-        var_dump($list);
-        ?>
-    </ol>
+
     <?php
     // data 디렉토리에 있는 파일의 목록을 가져오고
     // 그 파일의 목록 하나하나를 li와 a 태그로 글목록 만들기
-    <ol>
-    $list = scandir('data');
-    $i = 0;
-    while($i<count($list)) {
-        if($list[$i]!= '.') {
-            if($list[$i] != '..') {
-                ?>
-                <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
-                <?php
-            }
-        }
+    
+    $list = scandir('./data');
+    $i=0;
+    while($i < 6) {
+        echo "<li>$list[$i]</li>\n";
         $i = $i + 1;
     }
     ?>
-    </ol>
-    ?>
-    <h2>
-        <?php
-        if(isset($_GET['id'])) {
-            echo $_GET['id'];
-        } else {
-            echo "welcome";
-        }
-        ?>
-    </h2>
-    <?php 
-    if(isset($_GET['id'])) {
-        echo file_get_conGtents("data/".$_GET['id']);
-    } else {
-        echo "hello, php";
-    }
-    ?>
+
 </body>
 </html>
