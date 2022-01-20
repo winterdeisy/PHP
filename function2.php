338fa3d2a89071e8aa1e9cc7653811cda9bc976e
chaewon <doingengineer@gmail.com>
2022-01-20 23:25:43 +0900

    function4

--- function2.php
+++ function2.php
@@ -40,9 +40,10 @@
         return $left+$right;
     }
     print(sum2(2,4));
-    file_put_contents('result.txt', sum2(2,4));
+    file_put_contents('result.txt', sum2(5,9));
     // email('egoing@egoing.net', sum2(2,4));
     // upload('egoing.net', sum2(2,4));
+    print(sum2(3,10));
 
     ?>
 </body>
