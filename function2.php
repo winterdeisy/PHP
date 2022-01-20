b4bdb44773fa3834ffc1f986e9e5c8f1214a3d94
chaewon <doingengineer@gmail.com>
2022-01-20 23:22:06 +0900

    function2

--- function2.php
+++ function2.php
@@ -12,12 +12,36 @@
 
     <?php
     function basic() {
-        print("lorem ipsum dolor");
-        print("lorem ipsum dolor");
+        print("lorem ipsum dolor<br>");
+        print("lorem ipsum dolor<br>");
 
     }    
     basic();
     basic();
+    basic();
+    basic();
+    ?>
+    <h2>parameter &amp; argument</h2>
+    <?php
+    function sum($left, $right) {
+        print($left+$right);
+        print("<br>");
+
+    }
+    sum(2,4);
+    sum(4,6);
+    ?>
+
+    <h2>return</h2>
+    <?php
+    function sum2($left, $right) {
+        return $left+$right;
+    }
+    print(sum2(2,4));
+    file_put_contents('result.txt', sum2(2,4));
+    // email('egoing@egoing.net', sum2(2,4));
+    // upload('egoing.net', sum2(2,4));
+
     ?>
 </body>
 </html>
