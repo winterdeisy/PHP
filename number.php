18c2e5e46ca37830e4d464a0e919b01ab023f3dc
chaewon <doingengineer@gmail.com>
2022-01-22 21:07:49 +0900

    number

---
+++ number.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>1+1</h1>
+<?php
+echo 1+1; 
+?>    
+<h1>2-1</h1>
+<?php
+echo 2-1;
+?>
+<h1>2*1</h1>
+<?php
+echo 2*2
+?>
+<h1>4/2</h1>
+<?php
+echo 4/2
+?>
+
+</body>
+</html>
