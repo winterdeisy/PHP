ceda3f173f88b01efc30722c5c0bc83582f794ce
chaewon <doingengineer@gmail.com>
2022-01-22 21:44:13 +0900

    parameter

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-    <?php
-    echo hello;
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+        echo $_GET['id'];
+        ?>
+    </h2>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quibusdam repellendus harum quo sequi hic asperiores reprehenderit rerum magnam quas eveniet aliquam possimus, consequuntur fugit voluptas cum id saepe. Perspiciatis.
+
+</body>
+</html>
