494b9f46c58960c0b2d7f1ad2d62f0f2ed3af22f
chaewon <doingengineer@gmail.com>
2022-01-22 21:27:02 +0900

    variable

---
+++ variable.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Variable</h1>
+    <?php
+    $name = "leezche";
+    echo "Lorem ipsum, dolor sit amet consectetur ".$name." adipisicing elit. Excepturi maiores ut numquam tempora facilis autem laboriosam inventore aliquam accusamus nobis iste non, ratione obcaecati reiciendis ab dolor magnam deserunt quaerat!"
+    ?>
+</body>
+</html>
