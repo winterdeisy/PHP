32b097c4fc8c64d32ed525515ec3b5b808d9aa53
chaewon <doingengineer@gmail.com>
2022-01-25 23:15:37 +0900

    scandir

--- index.php
+++ index.php
@@ -10,17 +10,40 @@
     <h1><a href="index.php">WEB</a></h1>
     <ol>  
         <?php
+        /*
         //scandir은 특정한 디렉토리에 소속된 파일을 배열로 반환한다.
         $list = scandir('./data');
         var_dump($list);
+        */
 
         //원소를 하나씩 꺼내오기
-        echo "<li>$list[0]</li>\n";
-        echo "<li>$list[1]</li>\n";
+        /*
+        echo "<li>$list[0]</li>\n"; //.
+        echo "<li>$list[1]</li>\n"; //..
         echo "<li>$list[2]</li>\n";
         echo "<li>$list[3]</li>\n";
         echo "<li>$list[4]</li>\n";
         echo "<li>$list[5]</li>\n";
+        */
+
+        //반복문 사용하기
+        /*
+        $i = 0;
+        while($i < 6) {
+            echo "<li>$list[$i]</li>\n";
+            $i = $i + 1;
+        }
+        */
+
+        //항목이 자동으로 추가되도록 반복문 사용하기
+        $list = scandir('./data');
+        $i = 0;
+        while($i < count($list)) {
+            echo "<li>$list[$i]</li>\n";
+            $i = $i + 1;
+        }
+
+
 
         ?>
     </ol>
