dcbc02be2dab97125b3972b60226d27af173f1b5
chaewon <doingengineer@gmail.com>
2022-01-28 23:11:34 +0900

    function2

---
+++ function2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <h2>Basic</h2>
+    <?php
+    //약속된 기호
+    function basic() {
+        print("lorem ipsum dolor");
+        print("lorem ipsum dolor");
+    }
+
+    basic();
+    basic();
+    ?>
+</body>
+</html>
