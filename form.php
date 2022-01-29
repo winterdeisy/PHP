a4906cc9cf3ecab7288835da3cfccf00083ef90c
chaewon <doingengineer@gmail.com>
2022-01-29 14:22:40 +0900

    form

--- form.php
+++ form.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
-echo "<p>title : ".$_GET['title']."</p>";
-echo "<p>description : ".$_GET['description']."</p>";
+file_put_contents('data/'.$_POST['title'], $_POST['description']);
 
 ?>    
-</body>
-</html>
 
